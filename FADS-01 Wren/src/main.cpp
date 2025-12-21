986d55748e482fb7096be171bbfc7c1c78ed6f59
adkadt <adamkb2004@gmail.com>
2025-12-21 00:22:48 -0500

    added bmp updates to main

--- FADS-01 Wren/src/main.cpp
+++ FADS-01 Wren/src/main.cpp
@@ -1,23 +1,48 @@
 #include <Arduino.h>
+#include <optional>
 #include "GpsManager.h"
+#include "BmpManager.h"
 
 // GPS using Serial4 pins (TX4 17, RX4 16)
-#define GPS_SERIAL Serial4
+#define GPS_SERIAL Serial5
 
 GpsManager gps(GPS_SERIAL, 115200);
+BmpManager bmp;
+
+double zero_altitude;
 
 void setup() {
-    Serial.begin(115200);      // USB Serial for Monitor
+    Serial.begin(115200);    // USB Serial for Monitor
+    while (!Serial) delay(10);      // Wait for Serial Monitor to open [REMOVE FOR FLIGHT]
     gps.Begin();    // starts gps
+
+    if (!bmp.Begin()) {
+        Serial.println("--BMP FAILED TO START--");
+    }
+    bmp.SetMode(BMP5XX_POWERMODE_CONTINUOUS, BMP5XX_ODR_240_HZ);
+    bmp.SetTempOSR(BMP5XX_OVERSAMPLING_2X);
+    bmp.SetPressureOSR(BMP5XX_OVERSAMPLING_16X);
+
+    while (!bmp.Update()) {
+        delay(10);
+    }
+    bmpData bmp_data = bmp.GetBmpData();
+    zero_altitude = bmp_data.altitude;
+
     Serial.println("Teensy 4.1 GPS Initialized");
 }
 
 int last_gps_time = 0;
 double sps;
 
+
+
 void loop() {
+    bool update = false;
+    
     // updates gps and if sucessful prints data
     if (gps.Update()) {
+        update = true;
         locationData gps_data = gps.GetLocationData();
         
         if (gps_data.is_valid) {
@@ -31,6 +56,16 @@ void loop() {
             Serial.printf("GPS SPS: %.2f\n", sps);
         }
         last_gps_time = millis();
+    }
+
+    if (bmp.Update()) {
+        update = true;
+        bmpData bmp_data = bmp.GetBmpData();
+        Serial.printf("Pressure: %.1fhPa, Altitude %.2fm, Temperature: %.1fC\n", bmp_data.pressure, bmp_data.altitude-zero_altitude, bmp_data.temperature);
+    }
+
+    if (update) {
+        Serial.println("==================");
     }
 
 }
