abc027e5aab282d30c55c416debfc45eafa52c3f
Adam Brandt <57259688+adkadt@users.noreply.github.com>
2026-05-18 18:27:23 -0400

    moved state logic

--- AVES-00 Shrike/src/main.cpp
+++ AVES-00 Shrike/src/main.cpp
@@ -3,21 +3,38 @@
 #include "BmpManager.h"
 #include "Altimeter.h"
 
-#define BOOST_TIME 4 // seconds to ignore pressure for apogee readings
+#define BOOST_TIME_MS 4000 // seconds to ignore pressure for apogee readings
 #define DROGUE_DROP_M 3 // the meters vertically down from apogee to deploy drogue chute
+#define FIRE_TIME_MS 2000
+#define MAIN_DEPLOYMENT_ALT 150 // meters AGL to deploy main chute at
+
+#define DROGUE_PIN 13
+#define MAIN_PIN 13
 
 enum class State: uint8_t {
     PAD_IDLE,
     BOOST,
     COAST,
     DROGUE_DEPLOYMENT,
+    DROGUE_DESCENT,
     MAIN_DEPLOYMENT,
+    MAIN_DESCENT,
     LANDED
 };
 State current_state = State::PAD_IDLE;
 
 BmpManager bmp;
 Altimeter altimeter;
+
+void handlePadIdle(double agl);
+void handleBoost(double agl);
+void handleCoast(double agl);
+void handleDrogueDeployment(double agl);
+void handleDrogueDescent(double agl);
+void handleMainDeployment(double agl);
+void handleMainDescent(double agl);
+void handleLanded(double agl);
+void transitionState(State next_state);
 
 void setup() {
     Serial.begin(115200);    // USB Serial for Monitor
@@ -35,10 +52,11 @@ void setup() {
     altimeter.setGroundMode(true);
 }
 
-double apogee = 0.0;
-
-double boostStart_ms;
 int stateCounter = 0;
+bool stateInit = false;
+unsigned long stateStart_ms = 0;
+
+double apogee = 0.0;
 
 void loop() {
     bool bmp_update = false;
@@ -47,68 +65,21 @@ void loop() {
     BmpData raw_bmp_data;
     if (bmp.update()) {
         raw_bmp_data = bmp.getBmpData();
-        bmp_update = true;
-    }
-
-    if (bmp_update) {
         altimeter.update(raw_bmp_data.altitude);
-        double agl = altimeter.getAltitudeAGL();
+    }
+    double agl = altimeter.getAltitudeAGL();
 
-        // enter current state
-        switch (current_state) {
-            case State::PAD_IDLE:
-                if (agl > 15) {
-                    stateCounter++;
-                    if (stateCounter >= 3) {
-                        current_state = State::BOOST;
-                        boostStart_ms = millis();
-                        stateCounter = 0;
-                        Serial.println("STATE-CHANGE: BOOST");
-                    }
-                } else {
-                    stateCounter = 0;
-                }
-                break;
-
-            case State::BOOST:
-                if (millis() - boostStart_ms > BOOST_TIME*1000) {
-                    current_state = State::COAST;
-                    Serial.println("STATE-CHANGE: COAST");
-                }
-                break;
-
-            case State::COAST:
-                if (agl > apogee) {
-                    apogee = agl;
-                }
-
-                if (agl < (apogee - DROGUE_DROP_M)) {
-                    stateCounter++;
-
-                    if (stateCounter >= 3) {
-                        // deploy drogue
-                        stateCounter = 0;
-                        current_state = State::DROGUE_DEPLOYMENT;
-                        Serial.println("STATE-CHANGE: DROGUE_DEPLOYMENT");
-                    }
-                } else {
-                    stateCounter = 0;
-                }
-
-                break;
-
-            case State::DROGUE_DEPLOYMENT:
-                break;
-            case State::MAIN_DEPLOYMENT:
-                break;
-            case State::LANDED:
-                break;
-            default:
-                break;
-        }
-
-        // push info to flash or SD card
-
+    // enter current state
+    switch (current_state) {
+        case State::PAD_IDLE:               handlePadIdle(agl);             break;
+        case State::BOOST:                  handleBoost(agl);               break;
+        case State::COAST:                  handleCoast(agl);               break;
+        case State::DROGUE_DEPLOYMENT:      handleDrogueDeployment(agl);    break;
+        case State::DROGUE_DESCENT:         handleDrogueDescent(agl);       break;
+        case State::MAIN_DEPLOYMENT:        handleMainDeployment(agl);      break;
+        case State::MAIN_DESCENT:           handleMainDescent(agl);         break;
+        case State::LANDED:                 handleLanded(agl);              break;
+        default:                            break;
     }
 
     static unsigned long last_ping = 0;
@@ -116,4 +87,99 @@ void loop() {
         last_ping = millis();
         Serial.printf("Alive Ping: %d\n", millis() / 1000);
     }
+}
+
+// ---------------------
+// State Logic Functions
+// ---------------------
+
+void handlePadIdle(double agl) {
+    if (agl > 15) {
+        stateCounter++;
+        if (stateCounter >= 3) {
+            transitionState(State::BOOST);
+        }
+    } else {
+        stateCounter = 0;
+    }
+}
+
+
+void handleBoost(double agl) {
+    if (millis() - stateStart_ms > BOOST_TIME_MS) {
+        transitionState(State::COAST);
+    }
+}
+
+
+void handleCoast(double agl) {
+    if (agl > apogee) {
+        apogee = agl;
+    }
+
+    if (agl < (apogee - DROGUE_DROP_M)) {
+        stateCounter++;
+        if (stateCounter >= 3) {
+            transitionState(State::DROGUE_DEPLOYMENT);
+        }
+    } else {
+        stateCounter = 0;
+    }
+}
+
+
+void handleDrogueDeployment(double agl) {
+    if (!stateInit) {
+        digitalWrite(DROGUE_PIN, HIGH);
+        stateStart_ms = millis();
+        stateInit = true;
+
+    } else if (millis() - stateStart_ms >= FIRE_TIME_MS) {
+        digitalWrite(DROGUE_PIN, LOW);
+        transitionState(State::DROGUE_DESCENT);
+    }
+}
+
+
+void handleDrogueDescent(double agl) {
+    if (agl < MAIN_DEPLOYMENT_ALT) {
+        stateCounter++;
+        if (stateCounter >= 3) {
+            transitionState(State::MAIN_DEPLOYMENT);
+        }
+    } else {
+        stateCounter = 0;
+    }
+}
+
+
+void handleMainDeployment(double agl) {
+    if (!stateInit) {
+        digitalWrite(MAIN_PIN, HIGH);
+        stateStart_ms = millis();
+        stateInit = true;
+
+    } else if (millis() - stateStart_ms >= FIRE_TIME_MS) {
+        digitalWrite(MAIN_PIN, LOW);
+        transitionState(State::MAIN_DESCENT);
+    }
+}
+
+
+void handleMainDescent(double agl) {
+
+}
+
+
+void handleLanded(double agl) {
+
+}
+
+void transitionState(State next_state) {
+    stateCounter = 0;
+    stateStart_ms = millis();
+    stateInit = false;
+
+    current_state = next_state;
+    Serial.printf("STATE-CHANGE: %d\n", static_cast<int>(next_state));
 }
